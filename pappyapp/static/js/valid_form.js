5e58f61d367dc3eb3cd5cf43fb0549fb3a68bb34
Frederic DESCHAMPS <fd-mail@laposte.net>
2020-07-31 17:28:41 +0200

    Ajout du spinner et de la validation par entree

--- pappyapp/static/js/valid_form.js
+++ pappyapp/static/js/valid_form.js
@@ -1,31 +1,37 @@
 $(document).ready(function(){
 
-    var $query = $('#query_text_form'),
-        $envoi = $('#query-envoi');
-        $reset = $('query-reset');
+    var $query = $('#query_text_form');
+        $reset = $('query_reset');
 
-    $envoi.on('click', function(e){
-        e.preventDefault();
+    $query.on('keydown', function(e){
 
-        if ($query.val() == "") {
-            alert("N'oublies pas de me poser une question avant de cliquer !")
-            return 0;
-        }
+        if(e.keyCode == 13){
+            e.preventDefault();
 
-        $.post( "/question", { query_text_form: $query.val() })
-          .done(function( data ) {
-            $('#response').html(data['response']);
-            $('#otherplaces').html(data['otherplaces']);
-            initMap(data['lat'], data['lng']);
-            $('#map').show();
-        });
+            if ($query.val() == "") {
+                alert("N'oublies pas de me poser une question avant de cliquer !")
+                return 0;
+            }
 
+            $('#query_reset').hide();
+            $('#spinner').show();
+            
+            $.post( "/question", { query_text_form: $query.val() })
+              .done(function( data ) {
+                $('#response').html(data['response']);
+                $('#otherplaces').html(data['otherplaces']);
+                initMap(data['lat'], data['lng']);
+                $('#map').show();
+                $('#spinner').hide();
+                $('#query_reset').show();
+            });
+        };
     });
 
     $reset.on('click', function(e){
         e.preventDefault();
         $('#response').html("");
         $('#map').hide();
-        $('#otherplaces').html("");
+        $('#otherplaces').html(""); 
     });
 });
